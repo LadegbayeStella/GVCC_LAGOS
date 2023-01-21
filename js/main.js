4dd4108eab119eb25ac7bfd4106b1c2936de5ec5
Ladegbaye Stella <ladegbayeewatomi50@gmail.com>
2023-01-21 10:39:15 +0100

    pushing to github

--- js/main.js
+++ js/main.js
@@ -134,3 +134,19 @@ jQuery(document).ready(function($) {
         }
     });
 });
+
+
+
+// <!-- GetButton.io widget -->
+(function () {
+    var options = {
+        whatsapp: "+2349064967544", // WhatsApp number
+        call_to_action: "Message us", // Call to action
+        button_color: "#FF6550", // Color of button
+        position: "right", // Position may be 'right' or 'left'
+    };
+    var proto = 'https:', host = "getbutton.io", url = proto + '//static.' + host;
+    var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+    s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+    var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+})();
